fd16daeef98281a8f80c4ac83beb5e57df1fdde2
Wilson Lu <huiquanlu@gmail.com>
2016-06-17 14:47:02 -0700

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-# 300.times do
-#   Post.create title: Faker::Book.title,
-#               body:  Faker::StarWars.quote
-# end
-
-["Sports", "Technology", "Finance", "Hardware", "Software", "Entertainment", "Politic", "Education", "Housing", "Games"].each do |c|
-  Category.create title: c
+300.times do
+  Post.create title: Faker::Book.title,
+              body:  Faker::StarWars.quote
 end
+
+# ["Sports", "Technology", "Finance", "Hardware", "Software", "Entertainment", "Politic", "Education", "Housing", "Games"].each do |c|
+#   Category.create title: c
+# end
