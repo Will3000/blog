33ce4b6b07850916f36f8fc34a810bcdfb43f7c1
Wilson Lu <huiquanlu@gmail.com>
2016-06-20 21:18:16 -0700

    add dropdown menu

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    session[:attempt] |= 0;
     user = User.find_by_email params[:email]
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: "Signed In!"
     else
+      session[:attempt] += 1;
       flash[:alert] = "Wrong credentials"
       render :new
     end
